8b5d6db644f7546868f420a87e5fba5beb427f2b
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-12 12:47:00 +0000

    types(config): create config interface in config.interface.ts

---
+++ src/app/interfaces/config.interface.ts
@@ -0,0 +1,12 @@
+export interface IEnvConfig {
+  NODE_ENV: string;
+  PORT: number;
+  DATABASE_URL: string;
+  JWT_SECRET: string;
+  JWT_EXPIRES: string;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
+  ALLOWED_ORIGINS: string[];
+}
+
