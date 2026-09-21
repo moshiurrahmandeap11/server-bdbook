4fd671517adb62deb7315121d3a2ae25a61eddbb
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-21 13:45:35 +0600

    feat(follow): define IFollowStatusResponse interface

--- src/app/module/follow/follow.interface.ts
+++ src/app/module/follow/follow.interface.ts
@@ -1,6 +1,22 @@
-export interface IFollow {
+export interface IFollowUser {
   id: string;
-  followerId: string;
-  followingId: string;
-  createdAt: Date;
+  _id: string;
+  fullName: string;
+  username: string | null;
+  email: string;
+  profilePicture: {
+    url: string | null;
+  } | null;
+  profilePicUrl: string | null;
+  bio: string | null;
+  followedAt: Date;
 }
+
+export interface IFollowStatusResponse {
+  isFollowing: boolean;
+}
+
+export interface IFollowCountResponse {
+  count: number;
+}
+
